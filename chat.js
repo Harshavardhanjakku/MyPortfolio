dba19afd016071f4dd9acbc1c2643526adfaa2f0
Harshavardhanjakku <2203a51012@sru.edu.in>
2025-06-16 10:18:27 +0530

    edited the chat.js

--- chat.js
+++ chat.js
@@ -1,5 +1,4 @@
-const BACKEND_URL = "https://harshavardhan-one.vercel.app/generate";
-
+const BACKEND_URL = "https://harshavardhan-84ul.onrender.com/generate";
 const chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
